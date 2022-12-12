de93a47768de8046166662dbcc417762c502f133
leejeongho <jh9628771@gmail.com>
2022-12-13 00:09:02 +0900

    chore: 라우터변경 및 스타일기본적용

---
+++ src/routes/Router.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import HomePage from '../pages/Home/HomePage';
+import LoginPage from '../pages/Login/LoginPage';
+import SignupPage from '../pages/Signup/SignupPage';
+import ProfilePage from '../pages/Profile/ProfilePage';
+import ErrorPage from '../pages/Error/ErrorPage';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path="/profile" element={<ProfilePage />}></Route>
+        <Route path="/error" element={<ErrorPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
