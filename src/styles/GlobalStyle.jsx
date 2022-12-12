618532986822fb6c178d88816660a51c19505cd4
leejeongho <jh9628771@gmail.com>
2022-12-12 23:48:50 +0900

    Style: GlobalStyle과 공통폰트사이즈 및 공통 컬러 추가

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -2,20 +2,52 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-    ${reset}
+  ${reset}
 
-    * {
+  * {
         box-sizing: border-box;
-    }
+  }
 
-    body {
+  html {
+      font-size: 62.5%;
+  }
+
+  body {
         font-family: "NexonGothic";
-    }
+  }
 
-    button {
-        font-family: inherit;
-        border: none;
-    }
+  button {
+    border: none;
+    padding: 0;
+    background-color: inherit;
+    color: inherit;
+    font: inherit;
+    cursor: pointer;
+  }
+  
+  input {
+    font: inherit;
+  }
+
+  textarea {
+    border: none;
+    overflow: auto;
+    outline: none;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    resize: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    cursor: pointer;
+  }
+  
+  ol, ul {
+    list-style: none;
+  }
 `;
 
 export default GlobalStyle;
--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,1 +1,23 @@
 // 공통적인 스타일 변수
+const fontSize = {
+  xsmall: '1rem',
+  small: '1.2rem',
+  medium: '1.4rem',
+  large: '1.6rem',
+  xlager: '1.8rem',
+  xxlarge: '2.4rem',
+};
+
+const colors = {
+  lightWhite: '#F9F8ED',
+  lightGreen: '#C4E3CB',
+  Green: '#6A9C78',
+  lemonYellow: '#FFF1BC',
+};
+
+const Theme = {
+  fontSize,
+  colors,
+};
+
+export default Theme;
