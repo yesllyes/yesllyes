6b22fe6395ba8173a7f141237e27b7677f6b56be
Seulki Yun <icstuckyi@gmail.com>
2022-12-13 11:37:49 +0900

    style: Top-basic-nav 작업, .eslintrc 설정 수정

---
+++ src/components/navbar/Styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledTopBasicNav = styled.div`
+  width: 390px;
+  height: 48px;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export default StyledTopBasicNav;
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import LgButton from '../../components/button/Button';
+import TopBasicNav from '../../components/navbar/Top-basic-nav';
 
 export default function HomePage() {
-  return <LgButton>안녕</LgButton>;
+  return (
+    <>
+      <LgButton>안녕</LgButton>
+      <TopBasicNav>topbasicnav</TopBasicNav>
+    </>
+  );
 }
