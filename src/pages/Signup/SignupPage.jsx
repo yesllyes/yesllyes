eb6b59a76d9872c4436eb0ef05272c46eb8bd3b8
Seulki Yun <icstuckyi@gmail.com>
2022-12-14 17:55:10 +0900

    style: SignupPage (회원가입 페이지) UI 구현 #17

--- src/pages/Signup/SignupPage.jsx
+++ src/pages/Signup/SignupPage.jsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import StyledSignupSection from './Styled';
+import Button from '../../components/button/Button';
 
 export default function SignupPage() {
-  return <div>SignupPage</div>;
+  return (
+    <StyledSignupSection>
+      <h1>이메일로 회원가입</h1>
+      <form action="" method="">
+        <div className="div-input">
+          <label htmlFor="email">이메일</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="이메일 주소를 입력해 주세요."
+            required
+          />
+        </div>
+        <div className="div-input">
+          <label htmlFor="password">비밀번호</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="비밀번호를 설정해 주세요."
+            required
+          />
+        </div>
+        <div className="div-submit">
+          <Button size="lg" status="disabled">
+            다음
+          </Button>
+        </div>
+      </form>
+    </StyledSignupSection>
+  );
 }
---
+++ src/pages/Signup/Styled.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import StyledLoginSection from './../Login/Styled';
+
+const StyledSignupSection = styled(StyledLoginSection)``;
+
+export default StyledSignupSection;
