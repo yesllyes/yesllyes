b62b7535ca935521eb09d42a188d2cf36fe39e23
syoungp273 <syoungp273@gmail.com>
2022-12-14 18:29:02 +0900

    style: LoginPage 화면 구현

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,5 +1,44 @@
 import React from 'react';
+import FullLogoShadow from '../../assets/image/full-logo-shadow.svg';
+import StyledLoginPage from './Styled';
 
-export default function LoginPage() {
-  return <div>LoginPage</div>;
+function LoginPage() {
+  return (
+    <StyledLoginPage>
+      <div>
+        <h1>
+          <img src={FullLogoShadow} alt="" />
+        </h1>
+        <section className="login-join-section">
+          <ul className="sns-login">
+            <li className="sns-login-li">
+              <button className="kakao-login"> 카카오톡 계정으로 로그인</button>
+            </li>
+            <li className="sns-login-li">
+              <button className="google-login">구글 계정으로 로그인</button>
+            </li>
+            <li className="sns-login-li">
+              <button className="facebook-login">
+                페이스북 계정으로 로그인
+              </button>
+            </li>
+          </ul>
+          <ul className="login-join-cont">
+            <li>
+              <a className="emailLogin" href="/">
+                이메일로 로그인
+              </a>
+            </li>
+            <li>
+              <a className="join" href="/">
+                회원가입
+              </a>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </StyledLoginPage>
+  );
 }
+
+export default LoginPage;
