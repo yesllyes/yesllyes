d6ddb483bf8d3bdb8ee1f6e8e73af72641d3c5a1
hansang <hansanghug04@naver.com>
2022-12-14 17:50:36 +0900

    style: FeedPage 스타일링 구현 및 라우팅 작업

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,6 +6,8 @@ import LoginPage from '../pages/Login/LoginPage';
 import SignupPage from '../pages/Signup/SignupPage';
 import ProfilePage from '../pages/Profile/ProfilePage';
 import ErrorPage from '../pages/Error/ErrorPage';
+import FeedPage from '../pages/FeedPage/FeedPage';
+import Search from '../pages/Search/Search';
 
 function Router() {
   return (
@@ -16,6 +18,9 @@ function Router() {
         <Route path="/signup" element={<SignupPage />}></Route>
         <Route path="/profile" element={<ProfilePage />}></Route>
         <Route path="/error" element={<ErrorPage />}></Route>
+        <Route path='/feed' element={<FeedPage />}></Route>
+        <Route path='/search' element={<Search />}></Route>
+      
       </Routes>
     </BrowserRouter>
   );
