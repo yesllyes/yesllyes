86734f2be1374af307f3d7097d39acdd8260bb31
leejeongho <jh9628771@gmail.com>
2022-12-15 11:46:56 +0900

    fix: 잘못된 Merge 반영 수정

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import StyledButton from './styled';
 
-<<<<<<< HEAD
 const Button = ({ size, children, status, onClick }) => (
   <StyledButton size={size} status={status} onClick={onClick}>
-=======
-const Button = ({ size, children }) => (
-  <StyledButton size={size} active={false}>
->>>>>>> Develop
     {children}
   </StyledButton>
 );
