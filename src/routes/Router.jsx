d38d6201d2ea4d1d6e4f690091ef87fb8be0e6a9
leejeongho <jh9628771@gmail.com>
2022-12-19 18:16:15 +0900

    fix: 라우터 경로 수정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -7,7 +7,7 @@ import SignupPage from '../pages/Signup/SignupPage';
 import HomeFeedPage from '../pages/HomeFeed/HomeFeedPage';
 import ProfileSignupPage from '../pages/ProfileSignup/ProfileSignupPage';
 import ErrorPage from '../pages/Error/ErrorPage';
-import MainProfile from '../pages/Profile/MainProfile';
+import MyProfilePage from '../pages/MyProfile/MyProfilePage';
 
 function Router() {
   return (
@@ -18,8 +18,7 @@ function Router() {
       <Route path="/homefeed" element={<HomeFeedPage />}></Route>
       <Route path="/profilesignup" element={<ProfileSignupPage />}></Route>
       <Route path="/error" element={<ErrorPage />}></Route>
-      <Route path="/mainprofile" element={<MainProfile />}></Route>
-
+      <Route path="/myprofile" element={<MyProfilePage />}></Route>
     </Routes>
   );
 }
