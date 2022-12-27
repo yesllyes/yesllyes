82be8ba65aa2c8555e21a6ec884356edf2fc8a29
leejeongho <jh9628771@gmail.com>
2022-12-28 02:55:47 +0900

    chore: UserInfo 데이터에 따라 UI반영이 되도록 설정

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import StyledUserInfo from './styled';
-import BasicProfile from '../../assets/icon/icon-basic-profile-small.svg';
+// import BasicProfile from '../../assets/icon/icon-basic-profile-small.svg';
 
-function UserInfo() {
+function UserInfo({ user }) {
   return (
     <StyledUserInfo>
-      <img className="basic-profile" src={BasicProfile} alt="" />
+      <img className="basic-profile" src={user.image} alt="유저프로필이미지" />
       <div>
-        <h3>애월읍 위니브 감귤농장</h3>
-        <span>@weniv_Mandarin</span>
+        <h3>{user.username}</h3>
+        <span>{user.accountname}</span>
       </div>
     </StyledUserInfo>
   );
 }
-
 export default UserInfo;
