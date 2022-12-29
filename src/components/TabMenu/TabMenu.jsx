c41b360a1e4818f7240118a46a2f5f21d82bf3c1
leejeongho <jh9628771@gmail.com>
2022-12-29 11:31:34 +0900

    feat: Profile로 폴더명 변경으로 인한 Router경로 수정 및 TabMenu경로 수정

--- src/components/TabMenu/TabMenu.jsx
+++ src/components/TabMenu/TabMenu.jsx
@@ -5,35 +5,40 @@ import iconMessageLarge from '../../assets/icon/icon-message-large.svg';
 import iconEdit from '../../assets/icon/icon-edit.svg';
 import iconUser from '../../assets/icon/icon-user.svg';
 import StyledTabMenu from './styled';
+import useAuthContext from '../../hooks/useAuthContext';
 
-const TabMenu = () => (
-  <StyledTabMenu>
-    <ul>
-      <li>
-        <Link to="/homefeed">
-          <img src={iconHome} alt="" />홈
-        </Link>
-      </li>
-      <li>
-        <Link to="/chatlist">
-          <img src={iconMessageLarge} alt="" />
-          채팅
-        </Link>
-      </li>
-      <li>
-        <Link to="/postupload">
-          <img src={iconEdit} alt="" />
-          게시물 작성
-        </Link>
-      </li>
-      <li>
-        <Link to="/myprofile">
-          <img src={iconUser} alt="" />
-          프로필
-        </Link>
-      </li>
-    </ul>
-  </StyledTabMenu>
-);
+const TabMenu = () => {
+  const { auth } = useAuthContext();
+
+  return (
+    <StyledTabMenu>
+      <ul>
+        <li>
+          <Link to="/homefeed">
+            <img src={iconHome} alt="" />홈
+          </Link>
+        </li>
+        <li>
+          <Link to="/chatlist">
+            <img src={iconMessageLarge} alt="" />
+            채팅
+          </Link>
+        </li>
+        <li>
+          <Link to="/postupload">
+            <img src={iconEdit} alt="" />
+            게시물 작성
+          </Link>
+        </li>
+        <li>
+          <Link to={`/profile/${auth.accountName}`}>
+            <img src={iconUser} alt="" />
+            프로필
+          </Link>
+        </li>
+      </ul>
+    </StyledTabMenu>
+  );
+};
 
 export default TabMenu;
