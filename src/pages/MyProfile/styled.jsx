c66a535d05c35f256d6f6cbeb7d6d80ac1ed03b9
leejeongho <jh9628771@gmail.com>
2022-12-29 11:02:29 +0900

    remove: MyProfilePage ProfilePage로 파일명 및 폴더명 수정

--- src/pages/MyProfile/styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const StyledMyProfile = styled.div`
-  width: 100%;
-  box-sizing: border-box;
-  border: 0.5px solid #dbdbdb;
-`;
-
-export default StyledMyProfile;
