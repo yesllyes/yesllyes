479cb69b39251413f513fbade38e4734e48d1274
hansang <hansanghug04@naver.com>
2022-12-29 09:04:44 +0900

    feat: UserList 컴포넌트 keyword에 맞는러유저 정보 불라오기 구현

---
+++ src/components/UserList/styled.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+const StyledUserList = styled.article`
+  .userList {
+    display: flex;
+    margin-bottom: 16px;
+    cursor: pointer;
+    img {
+      width: 42px;
+      height: 42px;
+    }
+  }
+  .userInfo {
+    margin-left: 12px;
+  }
+
+  .userInfo .username {
+    padding: 5px 0 6px;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 18px;
+  }
+
+  .userInfo .accountname {
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 15px;
+  }
+
+  .keyword {
+    color: ${({ theme }) => theme.colors.Green};
+  }
+`;
+
+export default StyledUserList;
