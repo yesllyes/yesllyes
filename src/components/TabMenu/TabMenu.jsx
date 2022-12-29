4d9b62f9c00ce4fec6f992595fa29594bd8e5f23
hansang <hansanghug04@naver.com>
2022-12-29 16:04:56 +0900

    feat: 버튼에 따른 src 연결 수정 및 alt 내용 추가

--- src/components/TabMenu/TabMenu.jsx
+++ src/components/TabMenu/TabMenu.jsx
@@ -6,31 +6,46 @@ import homeIconActive from '../../assets/icon/icon-home-fill.svg';
 import chatIcon from '../../assets/icon/icon-message-large.svg';
 import chatIconActive from '../../assets/icon/icon-message-green.svg';
 import uploadIcon from '../../assets/icon/icon-edit.svg';
-
 import profileIcon from '../../assets/icon/icon-user.svg';
 import profileIconActive from '../../assets/icon/icon-user-fill.svg';
+import useAuthContext from '../../hooks/useAuthContext';
 
 const TabMenu = () => {
   const location = useLocation();
   const { pathname } = location;
+  const { auth } = useAuthContext();
 
   return (
     <StyledTabMenu>
       <StlyedNavLink to="/homefeed">
-        <img src={pathname === '/homefeed' ? homeIconActive : homeIcon} />
+        <img
+          src={pathname === '/homefeed' ? homeIconActive : homeIcon}
+          alt="홈페이지 이동 이미지 버튼"
+        />
         <p>홈</p>
       </StlyedNavLink>
-      <StlyedNavLink to="/">
-        <img src={pathname === '/' ? chatIconActive : chatIcon} />
+      <StlyedNavLink to="/chatlist">
+        <img
+          src={pathname === '/chatlist' ? chatIconActive : chatIcon}
+          alt="채팅 이동 이미지 버튼"
+        />
         <p>채팅</p>
       </StlyedNavLink>
       <StlyedNavLink to="/postupload">
-        <img src={pathname === '/postupload' ? uploadIcon : uploadIcon} />
+        <img
+          src={pathname === '/postupload' ? ' ' : uploadIcon}
+          alt="게시물 작성 이동 이미지 버튼"
+        />
         <p>게시물 작성</p>
       </StlyedNavLink>
-      <StlyedNavLink to="/myprofile">
+      <StlyedNavLink to={`/profile/${auth.accountName}`}>
         <img
-          src={pathname === '/myprofile' ? profileIconActive : profileIcon}
+          src={
+            pathname === `/profile/${auth.accountName}`
+              ? profileIconActive
+              : profileIcon
+          }
+          alt="프로필 정보 이동 이미지 버튼"
         />
         <p>프로필</p>
       </StlyedNavLink>
