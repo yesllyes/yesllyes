e2156038e037b68013f496e19fc92102e9acab2f
Seulki Yun <icstuckyi@gmail.com>
2022-12-30 16:13:22 +0900

    feat: PostUploadPage 유효성검사 추가

--- src/pages/Post/PostUploadPage.jsx
+++ src/pages/Post/PostUploadPage.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TopUploadNav } from '../../components/Navbar/TopNavbar';
 import useAuthContext from '../../hooks/useAuthContext';
-import { StyledPostUploadPage, StyledUploadContents } from './styled';
-import iconbasicprofilesmall from '../../assets/icon/icon-basic-profile-small.svg';
+import {
+  StyledPostUploadPage,
+  StyledUploadContents,
+  StyledUploadImg,
+} from './styled';
 
 const BASEURL = 'https://mandarin.api.weniv.co.kr';
 
@@ -11,18 +14,31 @@ function PostUploadPage() {
   const [postContent, setPostContent] = useState('');
   const [postImg, setPostImg] = useState(null);
 
-  const [isValidPostImg, setIsValidPostImg] = useState(false);
+  // onSubmit 전에 passed 체크 (빈 게시물이면 전송불가)
   const [isValidPostContent, setIsValidPostContent] = useState(false);
+  const [isValidPostImg, setIsValidPostImg] = useState(false);
 
-  // const [checkPostImg, setCheckPostImg] = useState('');
-  // const [checkPostContent, setCheckPostContent] = useState('');
+  // 유효성검사 (content: 1자 이상, img: 유효한 확장자만 허용)
+  const [checkPostContent, setCheckPostContent] = useState(0);
+  const [checkPostImg, setCheckPostImg] = useState(false);
 
   const navigate = useNavigate();
+  const countRef = useRef();
   const { auth } = useAuthContext();
-  const passed = isValidPostImg && isValidPostContent;
+  const passed =
+    (isValidPostContent && checkPostContent) ||
+    (isValidPostImg && checkPostImg);
+  const contentMaxLength = 330;
 
   const handleContentChange = (e) => {
     setPostContent(e.target.value);
+  };
+
+  const CheckLength = (e) => {
+    const txtLength = e.target.value.replace(/<br\s*\/?>/gm, '\n').length;
+
+    setIsValidPostContent(txtLength);
+    setCheckPostContent(txtLength);
   };
 
   const handleImgChange = (e) => {
@@ -32,22 +48,30 @@ function PostUploadPage() {
 
     formData.append('image', imgFile);
 
+    // 이미지 filename 응답 API(1개)
     const imgUpload = async () => {
-      // 이미지 filename 응답 API(1개)
       const ImgFetchResult = await fetch(`${BASEURL}/image/uploadfile`, {
         method: 'POST',
         // headers: {
-        //   Authorization: `Bearer ${auth.token}`,
         //   'Content-type': 'multipart/form-data',
         // },
         body: formData,
       });
-      const res = await ImgFetchResult.json();
+      const result = await ImgFetchResult.json();
 
-      // 이부분부터 3줄 이해안감
-      setPostImg(`${BASEURL}/${res.filename}`);
+      if (!result.filename) {
+        setPostImg(null);
+        setCheckPostImg(false);
+
+        alert(result.message);
+        return;
+      }
+
+      setPostImg(`${BASEURL}/${result.filename}`);
     };
 
+    setIsValidPostImg(true);
+    setCheckPostImg(true);
     imgUpload();
     setPostImg(imgFile);
   };
@@ -72,19 +96,12 @@ function PostUploadPage() {
 
     const result = await PostUpload.json();
 
-    // console.log(result);
     navigate(`/post/${result.post.id}`);
   };
 
-  // console.log(auth);
-
   return (
     <StyledPostUploadPage>
-      <TopUploadNav
-        // disabled={!passed}
-        onClick={onSubmit}
-        value="업로드"
-      />
+      <TopUploadNav disabled={!passed} onClick={onSubmit} value="업로드" />
       <StyledUploadContents>
         <h2 className="ir">게시글 작성</h2>
         <img src={auth.image} alt="" />
@@ -93,25 +110,44 @@ function PostUploadPage() {
           <form className="post-write-form">
             <textarea
               name="text"
-              placeholder="게시글 입력하기..."
+              placeholder="330자 이상 입력하실 수 없습니다."
               value={postContent}
               onChange={handleContentChange}
+              maxLength={contentMaxLength}
+              onKeyUp={CheckLength}
               required
             ></textarea>
             <label htmlFor="imgupload" className="campaign-img-label"></label>
             <input
+              className="ir"
               type="file"
               id="imgupload"
               accept="image/*"
               onChange={handleImgChange}
             />
           </form>
+          <span ref={countRef}>현재 {checkPostContent}자 작성</span>
         </article>
-        {/* 추가한 이미지 프리뷰 구현 */}
-        <h4 className="ir">추가한 이미지</h4>
-        {/* <ul>
-          <li><img src="" alt="" /></li>
-        </ul> */}
+        {postImg ? (
+          <StyledUploadImg>
+            <h4 className="ir">추가한 이미지</h4>
+            <ul>
+              <li>
+                <img
+                  src={postImg}
+                  alt="추가한 이미지"
+                  onClick={() => {
+                    setPostImg(null);
+                    setIsValidPostImg(false);
+                    setCheckPostImg(false);
+                  }}
+                />
+              </li>
+            </ul>
+          </StyledUploadImg>
+        ) : (
+          <></>
+        )}
       </StyledUploadContents>
     </StyledPostUploadPage>
   );
