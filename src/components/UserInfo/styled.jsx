bdd2e63544115041f1c5088cb36582d44c3c6929
hansang <hansanghug04@naver.com>
2022-12-30 13:46:31 +0900

    style: 프로필 이미지 원형으로 스타일링

--- src/components/UserInfo/styled.jsx
+++ src/components/UserInfo/styled.jsx
@@ -7,20 +7,28 @@ const StyledUserInfo = styled.article`
   gap: 12px;
   width: 358px;
 
-  .basic-profile {
+  .profile-box {
     width: 42px;
     height: 42px;
+    border-radius: 70%;
+    overflow: hidden;
+    border: 1px solid #dbdbdb;
+  }
+  .basic-profile {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 
-  div {
-    margin-top: 10px;
+  .user-name {
+    margin-top: 4px;
   }
 
   span {
     display: block;
     font-size: ${({ theme }) => theme.fontSize.small};
     color: ${({ theme }) => theme.colors.lightGray};
-    margin-top: 6px;
+    margin-top: 2px;
   }
 `;
 
