46b3d63b2a63e9ea9e07d8a4720629517fe8ba73
leejeongho <jh9628771@gmail.com>
2022-12-31 16:00:34 +0900

    fix: CampaignUploadPage 업로드 후 페이지 이동경로 및 CampaignCard 데이터렌더링 수정

--- src/components/CampaignCard/CampaignCard.jsx
+++ src/components/CampaignCard/CampaignCard.jsx
@@ -4,10 +4,12 @@ import CampaignCardWrapper from './styled';
 
 const CampaginCard = ({ campaign }) => (
   <CampaignCardWrapper>
-    <img src={campaign.itemImage} alt={campaign.itemName} />
+    <a href={campaign.link}>
+      <img src={campaign.itemImage} alt={campaign.itemName} />
+    </a>
     <p>{campaign.itemName}</p>
     <p>
-      <span>현재 진행 중</span>
+      <span>{`참가인원 ${campaign.price}명`}</span>
     </p>
   </CampaignCardWrapper>
 );
