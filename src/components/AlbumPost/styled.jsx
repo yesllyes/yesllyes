7f36b57616bdc08a934b877d2961cac8f159a9ae
leejeongho <jh9628771@gmail.com>
2022-12-31 16:02:11 +0900

    fix: PostPage 데이터렌더링 수정 및 AlbumPost컴포넌트 스타일 수정

--- src/components/AlbumPost/styled.jsx
+++ src/components/AlbumPost/styled.jsx
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
 
 const StyledAlbumCard = styled.article`
-  width: 114px;
-  height: 114px;
-  border: 1px solid ${({ theme }) => theme.colors.lightGray};
-  border-radius: 4px;
-  overflow: hidden;
+  > a {
+    display: block;
+    width: 114px;
+    height: 114px;
+    border: 1px solid ${({ theme }) => theme.colors.ActiveborderColor};
+    overflow: hidden;
+    border-radius: 4px;
+  }
+
   img {
     width: 100%;
+    height: 100%;
   }
 `;
 
