5f4ecd3bd715013a28d11009cb61b72b49efd7f2
syoungp273 <syoungp273@gmail.com>
2023-01-01 17:39:49 +0900

    style: Splash, IntoLoginPage 로고 수정

--- src/pages/Splash/SplashPage.jsx
+++ src/pages/Splash/SplashPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import fullLogo from '../../assets/image/full-logo-shadow.svg';
+import fullLogo from '../../assets/image/full-logo.svg';
 import StyledSplashPage from './styled';
 
 const SplashPage = () => (
   <StyledSplashPage>
     <h1 className="main-logo">
-      <img src={fullLogo} alt="fullLogoShadow" />
-      초록초록 예쁜 지구
+      <img src={fullLogo} alt="fullLogo" />
+      버드나다
     </h1>
   </StyledSplashPage>
 );
