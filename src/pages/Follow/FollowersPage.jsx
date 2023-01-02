8e7ba47dd6dffe3a536178e8a6cb93c2dd6c6099
hansang <hansanghug04@naver.com>
2023-01-02 10:57:28 +0900

    style: 팔로우 목록이 없을 시 페이지 스타일링 및 search 버튼 생성

--- src/pages/Follow/FollowersPage.jsx
+++ src/pages/Follow/FollowersPage.jsx
@@ -11,6 +11,8 @@ import {
   StyledFollowersList,
   TabMenuWrap,
 } from './styled';
+import LogoGray from '../../assets/image/full-logo-gray.svg';
+import Button from '../../components/Button/Button';
 
 const FollowersPage = () => {
   const [loading, setLoading] = useState(false);
@@ -20,6 +22,10 @@ const FollowersPage = () => {
   const location = useLocation();
   const { auth } = useAuthContext();
   const accountName = location.state.accountName;
+
+  const goSearch = () => {
+    window.location.href = '/search';
+  };
 
   useEffect(() => {
     setLoading(true);
@@ -57,7 +63,13 @@ const FollowersPage = () => {
       <StyledFollowersListWrapper>
         <StyledFollowersList>
           {!followers || followers.length === 0 ? (
-            <li>팔로우 목록이 존재하지 않습니다.</li>
+            <main className="non-post">
+              <img src={LogoGray} alt="회색이미지" />
+              <span>팔로우 목록이 존재하지 않습니다.</span>
+              <Button size="md" onClick={goSearch}>
+                검색하기
+              </Button>
+            </main>
           ) : (
             followers.map((follower) => (
               <FollowItem user={follower} key={follower._id} />
