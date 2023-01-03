9b5a9abfd3b5b9c2bdffe8dd18695820f2fe08bf
syoungp273 <syoungp273@gmail.com>
2023-01-04 00:14:21 +0900

    style: ChatRoomPage 화면 구현

--- src/pages/Chat/ChatRoomPage.jsx
+++ src/pages/Chat/ChatRoomPage.jsx
@@ -1,19 +1,61 @@
-import React from 'react';
-import { StyledChatPage, StyledChatRoomInput } from './styled';
+import React, { useState } from 'react';
+import {
+  StyledChatPage,
+  StyledChatTxtCont,
+  StyledChatRoomInput,
+} from './styled';
 import { TopChatNav } from '../../components/Navbar/TopNavbar';
-
 import Button from '../../components/Button/Button';
-
 import uploadImg from '../../assets/icon/icon-img-button-gray.svg';
+import BasicProfile from '../../assets/icon/icon-basic-profile-small.svg';
+import chatImg from '../../assets/image/chatImage.PNG';
 
 export default function ChatRoomPage() {
+  const [isValidChatTxt, setIsValidChatTxt] = useState(false);
+
+  const passed = isValidChatTxt;
+
+  const handleChangeChatTxt = (event) => {
+    const targetChatTxt = event.target.value;
+
+    setIsValidChatTxt(event.target.value);
+    if (targetChatTxt === '') {
+      setIsValidChatTxt(false);
+    } else {
+      setIsValidChatTxt(true);
+    }
+  };
+
   return (
     <StyledChatPage>
-      <TopChatNav value="Chat" />
+      <TopChatNav value="초록이" />
+      <StyledChatTxtCont>
+        <li>
+          <img className="basicProfile" src={BasicProfile} alt="" />
+          <p>이번 플로깅 행사 참여해두 될까요?</p>
+          <strong>11:24</strong>
+        </li>
+        <li>
+          <p>
+            그럼요! 자세한 사항은 제 프로필의 등록된 캠페인 글을 확인해주세요.
+          </p>
+          <img className="upload-img" src={chatImg} alt="" />
+          <strong>11:26</strong>
+        </li>
+        <li>
+          <img className="basicProfile" src={BasicProfile} alt="" />
+          <p>감사합니다!</p>
+          <strong>11:27</strong>
+        </li>
+      </StyledChatTxtCont>
       <StyledChatRoomInput>
         <img src={uploadImg} alt="" />
-        <input type="text" placeholder="메시지 입력하기..." />
-        <Button size="xsm" disabled>
+        <input
+          type="text"
+          placeholder="메시지 입력하기..."
+          onChange={handleChangeChatTxt}
+        />
+        <Button size="xsm" disabled={!passed}>
           전송
         </Button>
       </StyledChatRoomInput>
--- src/pages/Chat/styled.jsx
+++ src/pages/Chat/styled.jsx
@@ -5,7 +5,7 @@ import { StyledCommentInput } from '../../components/Comment/styled';
 export const StyledChatPage = styled.div`
   width: 100%;
   height: 820px;
-  border: 0.5px solid #dbdbdb;
+  border: 0.5px solid ${({ theme }) => theme.colors.ActiveborderColor};
   display: flex;
   flex-direction: column;
 
@@ -14,14 +14,83 @@ export const StyledChatPage = styled.div`
   }
 `;
 
+export const StyledChatTxtCont = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  font-size: ${({ theme }) => theme.fontSize.medium};
+  height: 711px;
+  background-color: #f2f2f2;
+
+  .basicProfile {
+    width: 42px;
+    height: 42px;
+    margin: 0 12px 0 16px;
+    border: 0.5px solid #dbdbdb;
+    border-radius: 50%;
+  }
+  li:nth-child(2) {
+    flex-direction: row-reverse;
+    flex-wrap: wrap;
+    margin-right: 16px;
+    p {
+      color: #ffffff;
+      background-color: ${({ theme }) => theme.colors.Green};
+      border-radius: 10px 10px 0 10px;
+    }
+    strong {
+      margin-top: auto;
+      margin-right: 10px;
+    }
+  }
+  li {
+    display: flex;
+    align-items: center;
+    margin-bottom: 9px;
+  }
+  strong {
+    margin-top: auto;
+    margin-left: 10px;
+    font-size: ${({ theme }) => theme.fontSize.xsmall};
+    color: ${({ theme }) => theme.colors.lightGray};
+  }
+  p {
+    padding: 17px 12px;
+    width: 240px;
+    border: 1px solid #c4c4c4;
+    border-radius: 10px 10px 10px 0;
+    background-color: #ffffff;
+    line-height: 16px;
+  }
+  .upload-img {
+    margin-top: 9px;
+    border: 1px solid #c4c4c4;
+    border-radius: 10px 10px 0 10px;
+  }
+`;
+
 export const StyledChatUserInfo = styled(StyledUserInfo)`
   width: 100%;
-  margin-top: 10px;
+  margin-top: 20px;
   padding: 10px 20px;
   display: flex;
   align-items: center;
   position: relative;
   box-sizing: border-box;
+
+  &.content::before {
+    position: absolute;
+    top: 0px;
+    content: '';
+    width: 12px;
+    height: 12px;
+    border-radius: 100%;
+    background-color: ${({ theme }) => theme.colors.Green};
+  }
+
+  li {
+    display: flex;
+  }
 
   .chat-user-cont {
     span {
