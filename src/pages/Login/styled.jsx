f885f2280b2a5a16a89fb915b0831c8801cfbc37
syoungp273 <syoungp273@gmail.com>
2023-01-04 00:26:16 +0900

    style: 전체 패딩, h1태그에 text-align, 에러메세지에 색상 부여

--- src/pages/Login/styled.jsx
+++ src/pages/Login/styled.jsx
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 const StyledLoginPage = styled.section`
   width: 100%;
   height: 820px;
+  padding: 60px 33px;
   border: 0.5px solid ${({ theme }) => theme.colors.ActiveborderColor};
 
   h1 {
     font-weight: 400;
     font-size: ${({ theme }) => theme.fontSize.xxlarge};
     margin-bottom: 40px;
+    text-align: center;
   }
 
   div {
@@ -26,6 +28,11 @@ const StyledLoginPage = styled.section`
   p {
     width: 320px;
     height: 10px;
+  }
+
+  .message {
+    color: ${({ theme }) => theme.colors.Orange};
+    font-size: ${({ theme }) => theme.fontSize.small};
   }
 `;
 
