fdd96a90021a1bbef1e55cd8754f21f5a95543dc
leejeongho <jh9628771@gmail.com>
2023-01-03 15:19:30 +0900

    feat: CampaginCard컴포넌트 Campaign모달창을 띄우기 위해서 연결 및 스타일 수정

--- src/components/CampaignCard/CampaignCard.jsx
+++ src/components/CampaignCard/CampaignCard.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import CampaignCardWrapper from './styled';
+import ModalButton from '../Modal/ModalButton';
 
 export default function CampaginCard({ campaign }) {
-  const navigate = useNavigate();
-
   return (
     <CampaignCardWrapper>
-      <img
-        src={campaign.itemImage}
-        alt={campaign.itemName}
-        // 아래 기능은 모달 적용 유무에 따라 처리 의논.
-        onClick={() => {
-          navigate(`/campaign/${campaign.id}/edit`, {
-            state: {
-              id: campaign.id,
-            },
-          });
-        }}
-      />
+      <ModalButton modalType="CampaignModal" campaign={campaign}>
+        <img src={campaign.itemImage} alt={campaign.itemName} />
+      </ModalButton>
       <p>{campaign.itemName}</p>
       <p>
-        <span>현재 진행 중</span>
+        <span>{`참가인원 ${campaign.price}명`}</span>
       </p>
     </CampaignCardWrapper>
   );
