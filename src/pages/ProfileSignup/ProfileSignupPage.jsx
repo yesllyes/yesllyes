4aad2bee1d32022f467d2afe47e6c2973dd5141a
syoungp273 <syoungp273@gmail.com>
2023-01-04 00:16:13 +0900

    chore: StyledWrapper삭제, 버튼 이름 서비스 이름으로 수정

--- src/pages/ProfileSignup/ProfileSignupPage.jsx
+++ src/pages/ProfileSignup/ProfileSignupPage.jsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import StyledWrapper from '../../components/Wrapper/StyledWrapper';
 import Button from '../../components/Button/Button';
 import TextInput from '../../components/TextInput/TextInput';
 import { StyledProfilePage, StyledImg, StyledTxt } from './styled';
@@ -130,64 +129,62 @@ export default function ProfileSignupPage() {
   // console.log('username', isValidUserName);
   // console.log('account', isValidAccountName);
   return (
-    <StyledWrapper>
-      <StyledProfilePage>
-        <h3>프로필 설정</h3>
-        <strong>나중에 언제든지 변경할 수 있습니다.</strong>
+    <StyledProfilePage>
+      <h3>프로필 설정</h3>
+      <strong>나중에 언제든지 변경할 수 있습니다.</strong>
 
-        <StyledImg action="post">
-          <img src={preImage || IconBasicProfile} alt="미리보기 이미지" />
-          <label htmlFor="imgupload" />
-          <input
-            className="ir"
-            type="file"
-            accept="image/*"
-            id="imgupload"
-            onChange={handleChangeImg}
-          />
-        </StyledImg>
-        <StyledTxt>
-          <TextInput
-            // 마크업, 스타일링에 필요한 속성
-            label="사용자 이름"
-            id="user"
-            placeholder="2~10자 이내로 작성해주세요."
-            // API 통신, 로직에 필요한 속성
-            name="username"
-            value={username}
-            onChange={handleChangeUserName}
-            // validation 에 필요한 속성 (로직 추가후 삭제예정)
-            type="text"
-            error={checkUserNameMsg ? 'error' : undefined}
-            required
-          />
-          <p>{checkUserNameMsg}</p>
-          <TextInput
-            label="계정 ID"
-            id="user"
-            placeholder="영문, 숫자, 특수문자(., _)만 사용 가능합니다."
-            name="accountname"
-            value={accountname}
-            onChange={handleChangeAccountName}
-            type="text"
-            error={checkAccountNameMsg ? 'error' : undefined}
-            required
-          />
-          <p>{checkAccountNameMsg}</p>
-          <TextInput
-            label="소개"
-            id="user"
-            placeholder="자유롭게 자신을 소개해주세요!"
-            name="intro"
-            value={intro}
-            onChange={handleChangeIntro}
-            type="text"
-          />
-          <Button size="lg" disabled={!passed} onClick={onSubmit}>
-            초록마켓 시작하기
-          </Button>
-        </StyledTxt>
-      </StyledProfilePage>
-    </StyledWrapper>
+      <StyledImg action="post">
+        <img src={preImage || IconBasicProfile} alt="미리보기 이미지" />
+        <label htmlFor="imgupload" />
+        <input
+          className="ir"
+          type="file"
+          accept="image/*"
+          id="imgupload"
+          onChange={handleChangeImg}
+        />
+      </StyledImg>
+      <StyledTxt>
+        <TextInput
+          // 마크업, 스타일링에 필요한 속성
+          label="사용자 이름"
+          id="user"
+          placeholder="2~10자 이내로 작성해주세요."
+          // API 통신, 로직에 필요한 속성
+          name="username"
+          value={username}
+          onChange={handleChangeUserName}
+          // validation 에 필요한 속성 (로직 추가후 삭제예정)
+          type="text"
+          error={checkUserNameMsg ? 'error' : undefined}
+          required
+        />
+        <p>{checkUserNameMsg}</p>
+        <TextInput
+          label="계정 ID"
+          id="user"
+          placeholder="영문, 숫자, 특수문자(., _)만 사용 가능합니다."
+          name="accountname"
+          value={accountname}
+          onChange={handleChangeAccountName}
+          type="text"
+          error={checkAccountNameMsg ? 'error' : undefined}
+          required
+        />
+        <p>{checkAccountNameMsg}</p>
+        <TextInput
+          label="소개"
+          id="user"
+          placeholder="자유롭게 자신을 소개해주세요!"
+          name="intro"
+          value={intro}
+          onChange={handleChangeIntro}
+          type="text"
+        />
+        <Button size="lg" disabled={!passed} onClick={onSubmit}>
+          버드나다 시작하기
+        </Button>
+      </StyledTxt>
+    </StyledProfilePage>
   );
 }
