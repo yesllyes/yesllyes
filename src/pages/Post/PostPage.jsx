0c80af062242c8de017cd7715e2fd448156c7ff7
leejeongho <jh9628771@gmail.com>
2023-01-03 15:49:44 +0900

    fix: PostPage에서 CommentData를 관리하도록 설정

--- src/pages/Post/PostPage.jsx
+++ src/pages/Post/PostPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { TopBasicNav } from '../../components/Navbar/TopNavbar';
 import CommentInput from '../../components/Comment/CommentInput';
@@ -8,31 +8,47 @@ import CommentList from './../../components/Comment/CommentList';
 import useAuthContext from '../../hooks/useAuthContext';
 
 export default function PostPage() {
+  const BASEURL = 'https://mandarin.api.weniv.co.kr';
+
   const { postId } = useParams();
   const { auth } = useAuthContext();
   const [postData, setPostData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [commentData, setCommentData] = useState(null);
 
-  useEffect(() => {
-    setLoading(true);
-    fetch(`https://mandarin.api.weniv.co.kr/post/${postId}`, {
+  const getPostData = useCallback(async () => {
+    const getPostReq = await fetch(`${BASEURL}/post/${postId}`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${auth.token}`,
         'Content-type': 'application/json',
       },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setPostData(res.post);
-        setLoading(false);
-      })
-      .catch((e) => new Error(e));
-  }, [postId, auth]);
+    });
+    const result = await getPostReq.json();
 
-  if (loading) {
-    <div>Loading중입니다.</div>;
-  }
+    setPostData(result.post);
+  }, [postId, auth.token]);
+
+  const getCommentData = useCallback(async () => {
+    const getCommentReq = await fetch(
+      `${BASEURL}/post/${postId}/comments?limit=999`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+          'Content-type': 'application/json',
+        },
+      }
+    );
+
+    const result = await getCommentReq.json();
+
+    setCommentData(result.comments);
+  }, [postId, auth.token]);
+
+  useEffect(() => {
+    getPostData();
+    getCommentData();
+  }, [getCommentData, getPostData]);
 
   return (
     <StyledWrapper>
@@ -40,8 +56,11 @@ export default function PostPage() {
       {postData ? (
         <TextPost key={postData._id} postData={postData}></TextPost>
       ) : null}
-      <CommentList />
-      <CommentInput />
+      <CommentList
+        commentData={commentData}
+        updateCommentData={getCommentData}
+      />
+      <CommentInput updateCommentData={getCommentData} />
     </StyledWrapper>
   );
 }
