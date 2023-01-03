a4ae1450e43d515a93983f4757d4820848fc6632
syoungp273 <syoungp273@gmail.com>
2023-01-04 00:06:21 +0900

    chore: 베이직프로필 이미지 변경, StyledChatUserInfo 컴포넌트에 className추가, 콘텐츠 내용 수정

--- src/pages/Chat/ChatListPage.jsx
+++ src/pages/Chat/ChatListPage.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import { StyledChatPage, StyledChatUserInfo } from './styled';
 import { TopBasicNav } from '../../components/Navbar/TopNavbar';
 import TabMenu from './../../components/TabMenu/TabMenu';
-import BasicProfile from '../../assets/icon/icon-user.svg';
+import BasicProfile from '../../assets/icon/icon-basic-profile-small.svg';
 
 export default function ChatListPage() {
   return (
     <StyledChatPage>
       <TopBasicNav />
       <Link to="/chatroom">
-        <StyledChatUserInfo>
+        <StyledChatUserInfo className="content">
           <div className="profile-box">
             <img
               src={BasicProfile}
@@ -22,7 +22,7 @@ export default function ChatListPage() {
             <h3>초록이</h3>
             <span>이번에 플로깅 행사 참여해두 될까요?</span>
           </div>
-          <span>2022.01.02</span>
+          <span>2023.01.02</span>
         </StyledChatUserInfo>
       </Link>
       <Link to="/chatroom">
@@ -52,7 +52,7 @@ export default function ChatListPage() {
           </div>
           <div className="chat-user-cont">
             <h3>함께, 나다</h3>
-            <span>친환경 키트 재고 아직 남아있으신가요오~??</span>
+            <span>친환경 키트 재고 문의로 연락드립니다!</span>
           </div>
           <span>2022.12.25</span>
         </StyledChatUserInfo>
